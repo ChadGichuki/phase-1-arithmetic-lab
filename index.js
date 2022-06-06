938d2d389143415e818a4d3f5f9cbdd015799148
richard gichuki <chadgichuki@gmail.com>
2022-06-06 15:57:04 +0300

    Done with math lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+// Math.ceil rounds up whichever random number to 1 which is greater than 0
+let random = Math.ceil(Math.random());
+
+
+const num3 = 28;
+const num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(17, 18, 19, 20);
